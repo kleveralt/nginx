f73ac6368c78c03b488ccb1899c452ef7b9373f1
root <klever1989@gmail.com>
2019-02-26 03:38:41 +0800

    chacha porioty

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -356,6 +356,10 @@ ngx_ssl_create(ngx_ssl_t *ssl, ngx_uint_t protocols, void *data)
     SSL_CTX_set_max_proto_version(ssl->ctx, TLS1_3_VERSION);
 #endif
 
+#ifdef SSL_OP_PRIORITIZE_CHACHA
+    SSL_CTX_set_options(ssl->ctx, SSL_OP_PRIORITIZE_CHACHA);
+#endif
+
 #ifdef SSL_OP_NO_COMPRESSION
     SSL_CTX_set_options(ssl->ctx, SSL_OP_NO_COMPRESSION);
 #endif
